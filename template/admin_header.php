cf9e75a8cad0041eb39b14b7231a2678b88e6e59
ItsWiggums <cerlangg@gmail.com>
2016-12-02 14:35:29 +0700

    Add files via upload

--- template/admin_header.php
+++ template/admin_header.php
@@ -14,8 +14,6 @@ $page = $url2[0];
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	<title><?php echo $title; ?></title>
-<!-- 	<script type="text/javascript" scr="assets/js/jquery-1.10.2.js"></script>
-	<script type="text/javascript" scr="assets/js/jquery-ui.min.js"></script> -->
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/jquery-ui.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
